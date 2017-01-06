ad769059a1a581b22ebb96f905fa48b0650f9787
marec <marecek.hroman@gmail.com>
2017-01-06 13:40:33 +0100

    Linq

---
+++ Dotazování Binárního Stromu/Dotazování Binárního Stromu/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dotazování_Binárního_Stromu
+{
+    class Employee : IComparable<Employee>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Deparement { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Id: {0}, Jméno: {1} {2}, Odd.: {3}",
+                    this.Id, this.Name, this.Surname, this.Deparement);
+        }
+
+        int IComparable<Employee>.CompareTo(Employee other)
+        {
+            if (other == null)
+                return 1;
+            if (this.Id > other.Id)
+                return 1;
+            if (this.Id < other.Id)
+                return -1;
+            return 0;
+        }
+    }
+}
--- Dotazování Binárního Stromu/Dotazování Binárního Stromu/Program.cs
+++ Dotazování Binárního Stromu/Dotazování Binárního Stromu/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BinaryTree;
 
 namespace Dotazování_Binárního_Stromu
 {
@@ -9,7 +10,91 @@ namespace Dotazování_Binárního_Stromu
     {
         static void Vstup()
         {
-            // dodělat
+            BTree<Employee> emplTree = new BTree<Employee>(
+                new Employee { Id = 1, Name = "Aneta", Surname = " Průšová", Deparement = "IT" });
+
+            emplTree.insert(new Employee
+            {
+                Id = 2,
+                Name = "Mirek",
+                Surname = " Travička",
+                Deparement = "Marketing"
+            });
+            emplTree.insert(new Employee
+            {
+                Id = 4,
+                Name = "Katka",
+                Surname = " Spěšná",
+                Deparement = "IT"
+            });
+            emplTree.insert(new Employee
+            {
+                Id = 6,
+                Name = "Lucie",
+                Surname = " Chvátalová",
+                Deparement = "Odbyt"
+            });
+            emplTree.insert(new Employee
+            {
+                Id = 3,
+                Name = "Erik",
+                Surname = " Doležal",
+                Deparement = "Odbyt"
+            });
+            emplTree.insert(new Employee
+            {
+                Id = 5,
+                Name = "David",
+                Surname = " Skála",
+                Deparement = "Marketing"
+            });
+
+            Console.WriteLine("Seznam oddeleni:");
+            //var department = emplTree.Select(d => d.Deparement).Distinct();
+            var department = (from d in emplTree
+                              select d.Deparement).Distinct();
+            foreach (var odd in department)
+            {
+                Console.WriteLine("Oddeleni: {0}", odd);
+            }
+
+            Console.WriteLine("\nZamestnanci IT:");
+            /*var itEmp = emplTree
+                .Where(emp => String.Equals(emp.Deparement, "IT"))
+                .Select(emp => emp);
+            */
+            var itEmp = from emp in emplTree
+                        where String.Equals(emp.Deparement, "IT")
+                        select emp;
+            foreach (var emp in itEmp)
+            {
+                Console.WriteLine("Zamestnanec: {0}", emp);
+            }
+
+            Console.WriteLine("\nOddeleni zamestnanci:");
+            /*var depEmp = emplTree
+                .GroupBy(z => z.Deparement);
+            */
+            var depEmp = from item in emplTree
+                         group item by item.Deparement;
+            foreach (var gr in depEmp)
+            {
+                Console.WriteLine("Oddeleni: {0}", gr.Key);
+                foreach (var emp in gr)
+                    Console.WriteLine("\t{0} {1}", emp.Name, emp.Surname);
+            }
+
+
+            Console.WriteLine("\n all staff:");
+            var empls = from e in emplTree.ToList<Employee>()
+                        select e;
+            foreach (var item in empls)
+                Console.WriteLine(item);
+
+            emplTree.insert(new Employee { Id = 7, Name = "Kamil", Surname = "Mrkvicka", Deparement = "IT" });
+            Console.WriteLine("\n all staff2:");
+            foreach (var item in empls)
+                Console.WriteLine(item);
         }
 
         static void Main()
